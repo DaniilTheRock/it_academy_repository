b29a9f18265d4a3ddbabdba9c2652ce3a3c072f8
Daniil Dobkin <daniiltherock@gmail.com>
2022-08-06 15:49:03 +0300

    Add payment servlet

---
+++ src/main/java/com/academy/controller/PaymentServlet.java
@@ -0,0 +1,18 @@
+package com.academy.controller;
+
+import com.academy.model.entity.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaymentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
